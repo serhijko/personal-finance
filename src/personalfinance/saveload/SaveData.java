93605d02e1ea95df50a9438d5caa61c85496ccb1
serhij <sp_by@tut.by>
2022-06-26 16:40:54 +0300

    Mechanism for saving and loading data

--- src/personalfinance/saveload/SaveData.java
+++ src/personalfinance/saveload/SaveData.java
@@ -8,8 +8,8 @@ import java.util.List;
 public class SaveData {
 
     private static SaveData instance;
-    private List<Article> articles = new ArrayList<>();
     private List<Account> accounts = new ArrayList<>();
+    private List<Article> articles = new ArrayList<>();
     private List<Currency> currencies = new ArrayList<>();
     private List<Transaction> transactions = new ArrayList<>();
     private List<Transfer> transfers = new ArrayList<>();
@@ -22,12 +22,12 @@ public class SaveData {
         return instance;
     }
 
-    public List<Article> getArticles() {
-        return articles;
-    }
-
     public List<Account> getAccounts() {
         return accounts;
+    }
+
+    public List<Article> getArticles() {
+        return articles;
     }
 
     public List<Currency> getCurrencies() {
@@ -40,6 +40,26 @@ public class SaveData {
 
     public List<Transfer> getTransfers() {
         return transfers;
+    }
+
+    public void setAccounts(List<Account> accounts) {
+        this.accounts = accounts;
+    }
+
+    public void setArticles(List<Article> articles) {
+        this.articles = articles;
+    }
+
+    public void setCurrencies(List<Currency> currencies) {
+        this.currencies = currencies;
+    }
+
+    public void setTransactions(List<Transaction> transactions) {
+        this.transactions = transactions;
+    }
+
+    public void setTransfers(List<Transfer> transfers) {
+        this.transfers = transfers;
     }
 
     public Currency getBaseCurrency() {
---
+++ src/personalfinance/saveload/SaveLoad.java
@@ -0,0 +1,47 @@
+package personalfinance.saveload;
+
+import personalfinance.settings.Settings;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+
+public class SaveLoad {
+
+    public void load(SaveData saveData) {
+        try {
+            JAXBContext context = JAXBContext.newInstance(Wrapper.class);
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+            Wrapper wrapper = (Wrapper) unmarshaller.unmarshal(Settings.getSaveFile());
+            saveData.setAccounts(wrapper.getAccounts());
+            saveData.setArticles(wrapper.getArticles());
+            saveData.setCurrencies(wrapper.getCurrencies());
+            saveData.setTransactions(wrapper.getTransactions());
+            saveData.setTransfers(wrapper.getTransfers());
+        } catch (JAXBException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public void save(SaveData saveData) {
+        try {
+            JAXBContext context = JAXBContext.newInstance(Wrapper.class);
+            Marshaller marshaller = context.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+            Wrapper wrapper = new Wrapper();
+
+            wrapper.setAccounts(saveData.getAccounts());
+            wrapper.setArticles(saveData.getArticles());
+            wrapper.setCurrencies(saveData.getCurrencies());
+            wrapper.setTransactions(saveData.getTransactions());
+            wrapper.setTransfers(saveData.getTransfers());
+
+            marshaller.marshal(wrapper, Settings.getSaveFile());
+        } catch (JAXBException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
